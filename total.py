23f5653ac23a50b740393321517ed85a583a277c
Piotr Muzyczuk <muzyczukp@me.com>
2018-12-15 22:23:54 +0100

    Fixes and first tests

--- total.py
+++ total.py
@@ -1,7 +1,9 @@
 import sanitizedata as sd
 from TrainingSet import TrainingSet
 from NeuralNetwork import NeuralNetwork
+from NetworkTester import NetworkTester
 import Functions
+from random import random
 
 if __name__ == '__main__':
     
@@ -11,14 +13,15 @@ if __name__ == '__main__':
     answers = []
     for mail in csv_output:
         answers.append([float(mail[0])])
-        data_vec = [float(mail[3])] # is formatted or not
+        data_vec = [] # is formatted or not
+        # data_vec = [float(mail[3])] # is formatted or not
         for topicPiece in mail[2]:
-            if(len(data_vec) >= 10): break
+            if(len(data_vec) >= 30): break
             data_vec.append(hash(topicPiece)/1000000000000000000) # topic piece per piece            
         for bodyPiece in mail[4]:
-            if(len(data_vec) >= 10): break
+            if(len(data_vec) >= 30): break
             data_vec.append(hash(bodyPiece)/1000000000000000000) # message
-        while len(data_vec) < 10:
+        while len(data_vec) < 30:
             data_vec.append(0)
         data.append(data_vec)
     
@@ -26,7 +29,20 @@ if __name__ == '__main__':
     print(data[3:4])
     print(answers[3:4])
 
-    training_set = TrainingSet(data, answers)
-    neural_network = NeuralNetwork(3, [3, 3, 1], [[1, -2, 3, 2, 1, 2, -1, 2, -2, -2, -1], [2, -1, -3, -1], [-1, -2, 2, 3]], [Functions.Sigmoid, Functions.Sigmoid, Functions.TanH], Functions.DiffSquare)
+    weight_vector = []
+    for i in range(31):
+        weight_vector.append(random()*10)
+
+    weight_vector2 = []
+    for i in range(11):
+        weight_vector2.append(random()*10)
+
+    training_set = TrainingSet(data[6:], answers[6:])
+    neural_network = NeuralNetwork(3, [10, 10, 1], [weight_vector, weight_vector2, weight_vector2], [Functions.Identity, Functions.Identity, Functions.Sigmoid], Functions.DiffSquare)
     #neural_network.make_guess(training_set.data[0])
-    neural_network.train(training_set, 0.1, 0.1)
+    neural_network.train(training_set, 0.1, 0.2)
+
+    for t in range(6):
+        print(neural_network.make_guess(training_set.data[t]))
+        print(answers[t])
+        print("--------")
